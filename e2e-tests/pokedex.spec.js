7c43c3b76c964d01576d58087aea808b1d1cdd7a
Marko Lähde <mtlahde@gmail.com>
2024-06-08 15:07:38 +0300

    New test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -1,8 +1,17 @@
 const { test, describe, expect } = require('@playwright/test')
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-     await page.goto('')
+    await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+  test('kakuna page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('kakuna')).toBeVisible()
+    await page.getByRole('link', { name: 'kakuna' }).click()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+    const link = await page.locator('a:has-text("Next")')
+    const href = await link.getAttribute('href')
+    expect(href).toBe('/pokemon/beedrill')
   })
 })
